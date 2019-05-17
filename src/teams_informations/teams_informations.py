b140d2c3c5fbe84284927eeb30428a1603005a00
JULIEN COHEN <julien.cohen1993@gmail.com>
2019-05-17 16:15:07 +0200

    [TEAMS][INFORMATIONS] - add venues and trophies section and format data in a dictionary

--- src/teams_informations/teams_informations.py
+++ src/teams_informations/teams_informations.py
@@ -17,8 +17,7 @@ def get_team_in_rank_table(soup):
     return teams_name, teams_link
 
 
-def get_team_info(request_team, teams_informations, teams_name, current_team):
-    soup = BeautifulSoup(request_team, 'html.parser')
+def get_team_info(soup):
     info = soup.find("div", {"class": "first-element"})
     dict_info = {}
     dl = info.find("dl")
@@ -37,10 +36,37 @@ def get_team_info(request_team, teams_informations, teams_name, current_team):
             else:
                 dict_info[list_dt[i].text.strip()] = list_dd[i].text.strip()
 
-    info_section = {}
-    info_section["Info"] = dict_info
-    teams_informations[teams_name[current_team]] = info_section
-    return teams_informations
+    info_section = {"Info": dict_info}
+    # teams_informations[teams_name[current_team]] = info_section
+    return info_section
+
+
+def get_team_venues_info(soup):
+    venues = soup.find("div", {"class": "second-element"})
+    dict_venue = {}
+    dl = venues.find("dl")
+    list_dt = dl.find_all("dt")
+    list_dd = dl.find_all("dd")
+    for i in range(len(list_dt)):
+        dict_venue[list_dt[i].text.strip()] = list_dd[i].text.strip()
+
+    return {"Venue": dict_venue}
+
+
+def get_team_trophies(soup):
+    dict_trophies_by_league = {}
+    venues = soup.find("div", {"class": "block_team_trophies_small-wrapper"})
+    trophies_table = venues.find("table", {"class": "table trophies"})
+    list_leagues = ["Premier League", "Championship", "League Cup", "FA Cup"]
+    for tr in trophies_table.find_all("tr"):
+        list_trophies_by_league = []
+        if tr.find_all("td"):
+            for td in tr.find_all("td"):
+                list_trophies_by_league.append(td.text)
+            if list_trophies_by_league[0] in list_leagues:
+                dict_trophies_by_league[list_trophies_by_league[0]] = list_trophies_by_league[2]
+
+    return {"Trophies": dict_trophies_by_league}
 
 
 def main():
@@ -51,15 +77,20 @@ def main():
         sys.stderr.write("enable to join the web site")
         return 1
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'lxml')
     teams_name, teams_links = get_team_in_rank_table(soup)
 
     current_team = 0
     for team_link in teams_links:
         # new request
         request_team = requests.get("https://us.soccerway.com/" + team_link)
-        teams_informations = get_team_info(request_team.text, teams_informations, teams_name, current_team)
+        soup = BeautifulSoup(request_team.text, 'lxml')
+        info_section = get_team_info(soup)
+        venues_section = get_team_venues_info(soup)
+        trophies_section = get_team_trophies(soup)
+        teams_informations[teams_name[current_team]] = [info_section, venues_section, trophies_section]
         current_team += 1
+
     print(teams_informations)
 
 
