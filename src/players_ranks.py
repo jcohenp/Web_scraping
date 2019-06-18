ab711862e0e4853a9a63ef8da9f5fb21bd4cb410
Rotem Sapir <rotemsap@gmail.com>
2019-06-18 19:36:55 +0300

    added id to top+players

--- src/players_ranks.py
+++ src/players_ranks.py
@@ -5,23 +5,22 @@ import config as config
 import pandas as pd
 
 
-
-
-def get_players(league_url):
+def get_players(league_url, player_id):
     """
     gets a league and returns the top players stats
     :param league_url: a specific league
     :return: list_players: a list with all the relevant info of the top players in the league
     """
+
     players_table = league_url.find("table", {"class": "playerstats table"})
-    # list_players = [["NAME", "TEAM", "GOALS", "FIRST GOALS"]]
     list_players = []
     for tr in players_table.tbody.find_all('tr'):
-        list_players.append(get_player_info(tr))
+        list_players.append(get_player_info(tr, player_id))
+        player_id += 1
     return list_players
 
 
-def get_player_info(tr):
+def get_player_info(tr, player_id):
     """
     take a player and get the stats of that player
     :param tr: a line in players table
@@ -31,12 +30,7 @@ def get_player_info(tr):
     team = tr.find('td', {"class": "team"})
     number_goals = tr.find('td', {"class": "number goals"})
     first_goals = tr.find('td', {"class": "number first-goals"})
-    # df2 = pd.DataFrame([[player.text.strip(), team.text.strip(), number_goals.text.strip(), first_goals.text.strip()]                        ], columns=['name', 'team', 'goals', 'first_goals'])
-    # top_players_pd.append(df2)
-    # print(top_players_pd)
-    return [player.text.strip(), team.text.strip(), number_goals.text.strip(), first_goals.text.strip()]
-    # top_players_pd.loc[i] = ['name' + str(i)] + list(randint(10, size=2))
-    # i += 1
+    return [player_id, player.text.strip(), team.text.strip(), number_goals.text.strip(), first_goals.text.strip()]
 
 
 def get_all_top_players_info():
@@ -51,25 +45,26 @@ def get_all_top_players_info():
         return -1
     soup = BeautifulSoup(general_website.text, 'lxml')
     list_leagues_url = config.get_leagues(soup)
-
+    player_id = 0
     dict_top_players_by_league = {}
     for league_url in list_leagues_url:
         league_url_last_season = league_url[0].rsplit("/", 2)
         league_url[0] = league_url_last_season[0] + "/20182019/" + league_url_last_season[2]
         res_league = requests.get(league_url[config.LEAGUE_URL])
-
         soup = BeautifulSoup(res_league.text, 'lxml')
-        league_players = get_players(soup)
+        league_players = get_players(soup, player_id)
         dict_top_players_by_league[league_url[config.LEAGUE_NAME]] = league_players
-
+        player_id += 15
     return make_dict_to_df(dict_top_players_by_league)
 
 
 def make_dict_to_df(dict_top_players_by_league):
     top_players_pd = pd.DataFrame()
     for key, value in dict_top_players_by_league.items():
-        df1 = pd.DataFrame(value, columns=['name', 'team', 'goals', 'first_goals'])
+
+        df1 = pd.DataFrame(value, columns=['player_id', 'name', 'team', 'goals', 'first_goals'])
         top_players_pd = pd.concat([top_players_pd, df1], axis=0, join='outer', join_axes=None, ignore_index=True,
                                    keys=None, levels=None,
                                    names=None, verify_integrity=False, copy=True)
+
     return top_players_pd
