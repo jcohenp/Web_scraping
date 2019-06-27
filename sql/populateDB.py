90028ea642209e7b891c028744e5148167c70af5
JULIEN COHEN <julien.cohen1993@gmail.com>
2019-06-27 15:50:41 +0200

    [SQL] - automate populate database

--- sql/populateDB.py
+++ sql/populateDB.py
@@ -1,6 +1,37 @@
 import csv
 import pymysql
-conn = pymysql.connect(host='127.0.0.1', user='root', passwd='root', db='soccerway', charset='utf8')
+
+
+def get_sql_from_file(filename):
+    """
+    Get the SQL instruction from a file
+
+    :return: a list of each SQL query whithout the trailing ";"
+    """
+    with open(filename, "r") as sql_file:
+        # Split file in list
+        ret = sql_file.read().split(';')
+        # drop last empty entry
+        ret.pop()
+        return ret
+
+
+request_list = get_sql_from_file("sql/soccerway.sql")
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='root')
+cur = conn.cursor()
+try:
+    cur.execute("CREATE database soccerway;")
+except pymysql.err.ProgrammingError:
+    cur.execute("DROP database soccerway;")
+    cur.execute("CREATE database soccerway;")
+finally:
+    cur.execute("USE soccerway;")
+
+for request in request_list:
+    cur.execute(request + ';')
+cur.close()
+
+conn = pymysql.connect(host='127.0.0.1', user='root', passwd='root', database='soccerway', charset='utf8')
 cur = conn.cursor()
 
 with open("CSV/teams_information.csv") as csvfile:
@@ -42,6 +73,4 @@ with open("CSV/top_player_info.csv") as csvfile:
         cur.execute("INSERT INTO top_players (id_player, name, team_id, goals, first_goals) VALUES (%s, %s, %s, %s, %s)", values)
 conn.commit()
 cur.close()
-
-
 
